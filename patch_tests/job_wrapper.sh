37855c18d237977ced2aa4bd67f13b9e9e7aafc8
Alexander Rogovskiy <alexander.rogovskiy@gmail.com>
2023-01-06 08:36:35 +0000

    Added files to allow one to run grid job to reproduce the error
    Namely, jdl and job script

---
+++ patch_tests/job_wrapper.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+#Grid job to reproduce "File name too long" error
+
+source /cvmfs/lhcb.cern.ch/group_login.sh
+
+#Copy file list -- just some random files with decent (approx 4GB each) size
+gfal-copy https://webdav.echo.stfc.ac.uk:1094/lhcb:user/lhcb/user/a/arogovsk/readv_stuff/listOfRandomRALFiles5 ./listOfRandomRALFiles
+
+for i in {1..6}; do
+   python ./readv_only_test.py -t border -u `shuf -n 1 ./listOfRandomRALFiles` -s &
+done
+
+job_ids=`jobs | grep -o "^\[[0-9]\+\]" | tr -d '[]'`
+wait `for i in $job_ids; do echo -n "%${i} "; done`
+rm listOfRandomRALFiles
