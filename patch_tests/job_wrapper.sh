ba3b022d2583885c655c879c0a0a46087d9e83e5
Alexander Rogovskiy <alexander.rogovskiy@gmail.com>
2023-01-06 09:40:20 +0000

    Fix

--- patch_tests/job_wrapper.sh
+++ patch_tests/job_wrapper.sh
@@ -2,10 +2,12 @@
 
 #Grid job to reproduce "File name too long" error
 
+echo "HOST: `hostname`"
+
 source /cvmfs/lhcb.cern.ch/group_login.sh
 
 #Copy file list -- just some random files with decent (approx 4GB each) size
-gfal-copy https://webdav.echo.stfc.ac.uk:1094/lhcb:user/lhcb/user/a/arogovsk/readv_stuff/listOfRandomRALFiles5 ./listOfRandomRALFiles
+lb-dirac gfal-copy https://webdav.echo.stfc.ac.uk:1094/lhcb:user/lhcb/user/a/arogovsk/readv_stuff/listOfRandomRALFiles5 ./listOfRandomRALFiles
 
 for i in {1..6}; do
    python ./readv_only_test.py -t border -u `shuf -n 1 ./listOfRandomRALFiles` -s &
