0334c61e0d836bb9106a8c6ef1ec0c474bb6f797
Alexander Rogovskiy <alexander.rogovskiy@gmail.com>
2023-01-16 08:51:31 +0000

    Update prod run infrastructure

---
+++ custom_production/create_container.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+docker run -d \
+    -v  "/etc/glexec.conf:/etc/glexec.conf:ro" \
+    -v "/etc/lcmaps:/etc/lcmaps:ro" \
+    -v "/etc/lcas:/etc/lcas:ro" \
+    -v "/lhcb_job:/lhcb_job" \
+    -v "/etc/grid-security:/etc/grid-security:ro" \
+    -v  "/etc/profile.d/grid-env.sh:/etc/profile.d/grid-env.sh:ro" \
+    -v "/cvmfs:/cvmfs:shared" \
+    -e "LOG_DIRECTORY=/lhcb_job/${2}" \
+    --network ralworker \
+    --name test_job_${1} \
+    b97af4dd7269 \
+    /bin/bash -c 'echo "172.28.1.1    xrootd.echo.stfc.ac.uk" >> /etc/hosts && su -c "/bin/bash -c \"cd /lhcb_job/repo/custom_production/output/'"${1}"' && ../../start_job.sh\"" --login lhcb001'
---
+++ custom_production/start_job.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+source /cvmfs/lhcb.cern.ch/group_login.sh
+export X509_USER_PROXY=/lhcb_job/proxy.pem
+export XRD_LOGLEVEL=Dump
+mkfifo ./stderr_fifo
+cat ./stderr_fifo | tee >( ../../../analyze/log2csv.py -o ./vector_times.csv -)  |  tail -n 10000 > last_std.err &
+lb-prod-run "prod_conf.json" --verbose 2> ./stderr_fifo
+retcode="$?"
+[ "$retcode" -eq 0 ] && { rm output.tuple.root; rm ./stderr_fifo; } || exit "$retcode"
