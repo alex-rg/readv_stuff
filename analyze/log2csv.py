f53909f2fc7a975081a38fbcdbdc55353e44acbe
Alexander Rogovskiy <alexander.rogovskiy@gmail.com>
2023-01-18 13:15:16 +0000

    feat: allow extraction of read operations

--- analyze/log2csv.py
+++ analyze/log2csv.py
@@ -11,6 +11,7 @@ def parse_arguments():
     parser = argparse.ArgumentParser()
     parser.add_argument('log', help='Path to log file that should be analized')
     parser.add_argument('-o', '--output', help='Path to the output file', default='./readv_times.csv')
+    parser.add_argument('-r', '--request_type', help='Type of the the request that should be extracted. Default is readv.', default='readv')
     return parser.parse_args()
 
 
@@ -30,17 +31,22 @@ def open_stdin():
     yield sys.stdin
 
 
-def log2csv(log_path, csv_path):
+def log2csv(log_path, csv_path, req_type='readv'):
     def _extract_key(match):
         time = timestamp2epoch(match.group('time'))
-        size = match.group('size')
-        chunks = match.group('chunks')
-        if chunks.endswith(']'):
-            chunks = len(chunks.split(';')) -1
-        key = (size, chunks)
+        if req_type == 'readv':
+            size = match.group('size')
+            chunks = match.group('chunks')
+            if chunks.endswith(']'):
+                chunks = len(chunks.split(';')) -1
+            key = (size, chunks)
+        elif req_type == 'read':
+            size = match.group('size')
+            offset = match.group('offset')
+            key = (size, offset)
         return (key, time)
 
-    readvs = {}
+    requests = {}
     if csv_path == '-':
         opener = open_stdout
     else:
@@ -54,12 +60,19 @@ def log2csv(log_path, csv_path):
     with opener() as csv_fd:
         with input_opener() as log_fd:
             timestamp_rexp = r'^\[(?P<time>[0-9]{4}-[0-9]{2}-[0-9]{2} [0-9:\.]+ \+[0-9]+)\]'
-            readv_data_rexp = r'\(handle: [0-9a-fx]+, chunks: (?P<chunks>[0-9 ,\[\]:;ofsetiz()]+), total size: (?P<size>[0-9]+)\)'
-            readv_send_rexp = r'Message kXR_readv {0} has been successfully sent'.format(readv_data_rexp)
-            readv_end_rexp = r'(?:Got a kXR_(?P<state>ok|error) response to request|Handling error while processing) kXR_readv {0}'.format(readv_data_rexp)
+            if req_type == 'readv':
+                req_data_rexp = r'\(handle: [0-9a-fx]+, chunks: (?P<chunks>[0-9 ,\[\]:;ofsetiz()]+), total size: (?P<size>[0-9]+)\)'
+                req_send_rexp = r'Message kXR_readv {0} has been successfully sent'.format(req_data_rexp)
+                req_end_rexp = r'(?:Got a kXR_(?P<state>ok|error) response to request|Handling error while processing) kXR_readv {0}'.format(req_data_rexp)
+            elif req_type == 'read':
+                req_data_rexp = r'\(handle: [0-9a-fx]+, offset: (?P<offset>[0-9]+), size: (?P<size>[0-9]+)\)'
+                req_send_rexp = r'Message kXR_read {0} has been successfully sent'.format(req_data_rexp)
+                req_end_rexp = r'(?:Got a kXR_(?P<state>ok|error) response to request|Handling error while processing) kXR_read {0}'.format(req_data_rexp)
+            else:
+                raise ValueError("Unsupported request type {0}: expected read or readv".format(req_type))
 
-            start_rexp = re.compile(r'{0}.*{1}'.format(timestamp_rexp, readv_send_rexp))
-            end_rexp = re.compile(r'{0}.*{1}'.format(timestamp_rexp, readv_end_rexp))
+            start_rexp = re.compile(r'{0}.*{1}'.format(timestamp_rexp, req_send_rexp))
+            end_rexp = re.compile(r'{0}.*{1}'.format(timestamp_rexp, req_end_rexp))
 
             chunk_rexp = re.compile(r'.*read buffer for chunk ([0-9@]+)$')
 
@@ -74,35 +87,40 @@ def log2csv(log_path, csv_path):
                     buf_start, buf_end = 10**100, -1
                     key, start = _extract_key(m)
                     try:
-                        readvs[key].append(start)
+                        requests[key].append(start)
                     except KeyError:
-                        readvs[key] = [start]
+                        requests[key] = [start]
 
-                m = chunk_rexp.match(line)
-                if m:
-                    size, pos = [int(x) for x in m.group(1).strip().split('@')]
-                    if size + pos > buf_end:
-                        buf_end = size + pos
-                    if pos < buf_start:
-                        buf_start = pos
-                    #print("bs={0}, be={1}".format(buf_start, buf_end), file=sys.stderr)
+                if req_type == 'readv':
+                    m = chunk_rexp.match(line)
+                    if m:
+                        size, pos = [int(x) for x in m.group(1).strip().split('@')]
+                        if size + pos > buf_end:
+                            buf_end = size + pos
+                        if pos < buf_start:
+                            buf_start = pos
+                        #print("bs={0}, be={1}".format(buf_start, buf_end), file=sys.stderr)
 
 
                 m = end_rexp.match(line)
                 if m:
                     key, end = _extract_key(m)
-                    if max_spread < buf_end - buf_start:
-                        max_spread = buf_end - buf_start
-                        best_buf_start, best_buf_end = buf_start, buf_end
-                        biggest_spread_request = key
+                    if req_type == 'readv':
+                        if max_spread < buf_end - buf_start:
+                            max_spread = buf_end - buf_start
+                            best_buf_start, best_buf_end = buf_start, buf_end
+                            biggest_spread_request = key
                     res = 0 if m.group('state') == 'ok' else 1
                     try:
-                        start = readvs[key].pop()
+                        start = requests[key].pop()
                     except (KeyError, IndexError):
-                        print("Found end of readv {0}, but can not find start! Probably log is incomplete, multiple error messages are present.".format(key), file=sys.stderr)
+                        print("Found end of request {0}, but can not find start! Probably log is incomplete, multiple error messages are present.".format(key), file=sys.stderr)
                     else:
                         lines_written += 1
-                        print('{0},{1},{2},{3},{4},{5}'.format(start, end - start, res, key[0],key[1], buf_end - buf_start), file=csv_fd)
+                        if req_type == 'readv':
+                            print('{0},{1},{2},{3},{4},{5}'.format(start, end - start, res, key[0],key[1], buf_end - buf_start), file=csv_fd)
+                        else:
+                            print('{0},{1},{2},{3},{4}'.format(start, end - start, res, key[0],key[1]), file=csv_fd)
                         if (lines_written + 1) % 5 == 0:
                             csv_fd.flush()
             print("Max spread = {0}, request = {1}, buf_start={2}, buf_end={3}".format(max_spread, key, best_buf_start, best_buf_end), file=sys.stderr)
@@ -110,4 +128,4 @@ def log2csv(log_path, csv_path):
 
 if __name__ == '__main__':
     args = parse_arguments()
-    log2csv(args.log, args.output)
+    log2csv(args.log, args.output, req_type=args.request_type)
