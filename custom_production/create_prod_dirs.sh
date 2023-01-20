16669d8c03ced4d7ba2670699c5d0bf1319aa950
Alexander Rogovskiy <alexander.rogovskiy@gmail.com>
2023-01-20 19:39:52 +0000

    Update for production creation scripts

--- custom_production/create_prod_dirs.sh
+++ custom_production/create_prod_dirs.sh
@@ -7,6 +7,6 @@ count=0
 for file in `find $dir -regextype egrep -regex '.*/[0-9]+$' -type f`; do
   job_dir="${out_dir}/${count}"
   mkdir "${job_dir}"
-  ${cdir}/create_prodfiles.py -p "${cdir}/prod_conf.json" -l "root://xrootd.echo.stfc.ac.uk:1095/lhcb:prod" -j "$file" -s "summary.xml" -o "${job_dir}/prod_conf.json"
+  ${cdir}/create_prodfiles.py -p "${cdir}/prod_conf.json" -l "root://xrootd.echo.stfc.ac.uk/lhcb:prod" -j "$file" -s "summary.xml" -o "${job_dir}/prod_conf.json"
   count=$((count + 1))
 done
--- custom_production/start_job.sh
+++ custom_production/start_job.sh
@@ -4,7 +4,7 @@ source /cvmfs/lhcb.cern.ch/group_login.sh
 export X509_USER_PROXY=/lhcb_job/proxy.pem
 export XRD_LOGLEVEL=Dump
 mkfifo ./stderr_fifo
-cat ./stderr_fifo | tee >( ../../../analyze/log2csv.py -o ./vector_times.csv -)  |  tail -n 10000 > last_std.err &
+cat ./stderr_fifo | tee >( ../../../analyze/log2csv.py -o ./vector_times.csv -) >(../../../analyze/log2csv.py -o ./read_times.csv -r read - ) | tail -n 10000 > last_std.err &
 lb-prod-run "prod_conf.json" --verbose 2> ./stderr_fifo
 retcode="$?"
-[ "$retcode" -eq 0 ] && { rm output.tuple.root; rm ./stderr_fifo; } || exit "$retcode"
+[ "$retcode" -eq 0 ] && rm ./stderr_fifo || exit "$retcode"
