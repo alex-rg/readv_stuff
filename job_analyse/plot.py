d3318f5b54e11dbc11f49f0686ab701134fbc3b6
Alexander Rogovskiy <tpw95647@lcgui05.gridpp.rl.ac.uk>
2023-08-11 16:17:30 +0100

    feat: allow plotting errors for job's analysis

--- job_analyse/plot.py
+++ job_analyse/plot.py
@@ -11,7 +11,7 @@ def parse_args():
     parser.add_argument('-o', '--output', help="Put output in a file", default='./output.png')
     parser.add_argument('-d', '--dpi', help="Image resolution, dpi", default=300, type=int)
     parser.add_argument('-t', '--title', help="Plot title", default=None)
-    parser.add_argument('-p', '--plot_type', help="Plot type", choices=['efficiency'], default='efficiency')
+    parser.add_argument('-p', '--plot_type', help="Plot type", choices=['efficiency', 'errors'], default='efficiency')
     parser.add_argument('-s', '--size', help="Picture size, in a form '<width>,<height>'", default=None)
     return parser.parse_args()
 
@@ -22,16 +22,22 @@ if __name__ == '__main__':
     res = []
     with open(args.data) as fd:
         for line in fd:
-            efficiency, item, all_jobs = line.split()
-            res.append( (item, float(efficiency)) )
+            efficiency, item, all_jobs, failures = line.split()
+            res.append( (item, float(efficiency), int(failures), int(all_jobs)-int(failures)) )
               
 
     if args.size:
         plt.figure(figsize=[int(x) for x in args.size.split(',')])
+    x = [k for k in range(len(res))]
     if args.plot_type == 'efficiency':
-        x = [k for k in range(len(res))]
         y = [v[1] for v in res]
         plt.bar(x, y)
+    elif args.plot_type == 'errors':
+        y = [v[2] for v in res]
+        y1 = [v[3] for v in res]
+        plt.bar(x, y, color='red')
+        plt.bar(x, y1, color='blue', bottom=y)
+        plt.legend(['fail', 'ok'])
     plt.xticks(x, [v[0] for v in res], rotation=90)
     if args.title is not None:
         plt.title(args.title)
