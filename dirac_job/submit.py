ea99cff69b687578e1df1797df8fca3dd387399e
Alexander Rogovskiy <arogovsk@lxplus970.cern.ch>
2025-02-20 18:04:38 +0100

    feat: add platform option to job submission script

--- dirac_job/submit.py
+++ dirac_job/submit.py
@@ -11,6 +11,7 @@ DEF_JOBNAME = "test_job"
 def parse_args():
     parser = argparse.ArgumentParser()
     parser.add_argument('-s', '--site', help="Site to run job on")
+    parser.add_argument('-p', '--platform', help="Platform to use. Default is any platform available", default=None)
     parser.add_argument('-n', '--name', help="Job name. Default is {0}".format(DEF_JOBNAME), default=DEF_JOBNAME)
     parser.add_argument('-m', '--multiple', help="Submit multiple jobs with the same script. Value gives the number of jobs to submit", type=int, default=1)
     parser.add_argument('-c', '--cpu_time', help="CPU Time for the job. Default is {0}".format(DEF_CPUTIME), default=DEF_CPUTIME, type=int)
@@ -22,19 +23,25 @@ if __name__ == '__main__':
     args = parse_args()
     j = Job()
     j.setCPUTime(args.cpu_time)
-    j.setExecutable(args.executable)
     if args.site:
         j.setDestination(args.site)
-    for i in range(args.multiple):
-        if args.multiple > 1:
-            jname = args.name + '_{0}'.format(i)
-        else:
-            jname = args.name
-        j.setName(jname)
 
-        dirac = Dirac()
-        result = dirac.submitJob(j)
-        if result['OK']:
-            print(result['Value'])
-        else:
-            print('Failed to submit: ',result)
+    if args.multiple > 1:
+        j.setInputSandbox(args.executable)
+        j.setParameterSequence("args", [x for x in range(args.multiple)])
+        j.setExecutable(args.executable, arguments="%(args)", logFile="app_log.log")
+        j.setOutputSandbox(['app_log.log', 'std.out', 'std.err'])
+    else:
+        j.setExecutable(args.executable)
+
+    if args.platform:
+        j.setPlatform(args.platform)
+
+    j.setName(args.name)
+
+    dirac = Dirac()
+    result = dirac.submitJob(j)
+    if result['OK']:
+        print(result['Value'])
+    else:
+        print('Failed to submit: ',result)
